30ade06a47d6fa5b76486e036c9848bb622f07c3
josecarrera26 <jose.luiscarrera199526@outlook.com>
2024-01-07 14:25:36 -0600

    first commit

---
+++ HR.LeaceMangement.Domain/LeaveAllocation.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaceMangement.Domain;
+using HR.LeaceMangement.Domain.Common;
+
+public class LeaveAllocation : BaseEntity
+{
+	public int NumberOfDays { get; set; }
+	public LeaveType? LeaveType { get; set; }
+	public int LeaveTypeId { get; set; }
+	public int Period { get; set; }
+}
---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,34 @@
+﻿using HR.LeaceMangement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence
+{
+	public interface IGenericRepository<T> where T : class
+	{
+		Task<T> GetAsync(int id);
+		Task<List<T>> GetByIdAsync();
+		Task<T> CreateAsync(T entity);
+		Task<T> UpdateAsync(T entity);
+		Task<T> DeleteAsync(T entity);
+
+	}
+
+	public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+	{
+
+	}
+
+	public interface ILeaveAllocationTypeRepository : IGenericRepository<LeaveAllocation>
+	{
+
+	}
+
+	public interface ILeaveRequestTypeRepository : IGenericRepository<LeaveRequest>
+	{
+
+	}
+}
